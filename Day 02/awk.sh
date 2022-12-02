9ae828e76d849b3395d062e3b0313e74114a06bc
bensteban <nasanerd@gmail.com>
2022-12-02 10:57:07 -0800

    Making consistent with cleaned up Day 01 convention

--- Day 02/awk.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-awk 'BEGIN{sum=0} { if($2=="X"){ playval=1; if($1=="A"){winval=3} else if($1=="B"){winval=0} else if($1=="C"){winval=6} } else if($2=="Y"){ playval=2; if($1=="A"){winval=6} else if($1=="B"){winval=3} else if($1=="C"){winval=0} } else if($2=="Z"){ playval=3; if($1=="A"){winval=0} else if($1=="B"){winval=6} else if($1=="C"){winval=3} }; sum = sum + playval + winval } END { print sum }' input.txt
---
+++ Day 02/part1.awk
@@ -0,0 +1,2 @@
+
+BEGIN{sum=0} { if($2=="X"){ playval=1; if($1=="A"){winval=3} else if($1=="B"){winval=0} else if($1=="C"){winval=6} } else if($2=="Y"){ playval=2; if($1=="A"){winval=6} else if($1=="B"){winval=3} else if($1=="C"){winval=0} } else if($2=="Z"){ playval=3; if($1=="A"){winval=0} else if($1=="B"){winval=6} else if($1=="C"){winval=3} }; sum = sum + playval + winval } END { print sum }
